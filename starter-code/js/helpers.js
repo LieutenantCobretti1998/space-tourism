c17be9341da46e7ec238e716d21aab005abced33
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-04-22 10:36:31 +0400

    refactoring phase!

--- starter-code/js/helpers.js
+++ starter-code/js/helpers.js
@@ -19,3 +19,26 @@ export  function DynamicImage() {
         return "portrait"
     }
 }
+
+// Hamburger Menu
+
+export function HamburgerMenu() {
+    const close_button_element = document.querySelector(".close_button");
+    close_button_element.addEventListener("click", () => {
+        console.log(1)
+        const menu_overlay_element = document.querySelector(".menu-overlay");
+        menu_overlay_element.classList.add("closing_menu");
+        menu_overlay_element.addEventListener('animationend', () => {
+            // Hide after animation completes
+            menu_overlay_element.classList.remove("active_menu");
+            menu_overlay_element.classList.remove('closing_menu'); // Clean up class to prepare for next open
+        }, { once: true });
+    })
+
+    const open_button_element = document.querySelector(".open_menu");
+    open_button_element.addEventListener("click", () => {
+        const menu_overlay_element = document.querySelector(".menu-overlay");
+        menu_overlay_element.classList.remove("closing_menu");
+        menu_overlay_element.classList.add("active_menu");
+    })
+}
