f87918973f509090257ede48a3d87294b7f7d32b
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-04-22 10:36:07 +0400

    some improvements in technologies

--- starter-code/js/technologies.js
+++ starter-code/js/technologies.js
@@ -1,6 +1,6 @@
 import PageHandler from "./init.js";
 import anime from "../../node_modules/animejs/lib/anime.es.js";
-import { DynamicImage } from "./helpers.js";
+import { DynamicImage, HamburgerMenu } from "./helpers.js";
 // Main animation init
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -38,21 +38,4 @@ technologies_numbers_element.addEventListener("click", async (e) => {
 
 
 // Hamburger Menu
-const close_button_element = document.querySelector(".close_button");
-close_button_element.addEventListener("click", () => {
-    console.log(1)
-    const menu_overlay_element = document.querySelector(".menu-overlay");
-    menu_overlay_element.classList.add("closing_menu");
-    menu_overlay_element.addEventListener('animationend', () => {
-        // Hide after animation completes
-        menu_overlay_element.classList.remove("active_menu");
-        menu_overlay_element.classList.remove('closing_menu'); // Clean up class to prepare for next open
-    }, { once: true });
-})
-
-const open_button_element = document.querySelector(".open_menu");
-open_button_element.addEventListener("click", () => {
-    const menu_overlay_element = document.querySelector(".menu-overlay");
-    menu_overlay_element.classList.remove("closing_menu");
-    menu_overlay_element.classList.add("active_menu");
-})
+HamburgerMenu();
